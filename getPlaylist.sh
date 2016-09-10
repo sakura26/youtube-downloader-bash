83809308072793d139c81d7ea2b88ca6b305c30d
sakura26 <sakura26@ccsakura-net.com>
2016-09-11 01:30:44 +0800

    add try-next for 403, seems not enough...

--- getPlaylist.sh
+++ getPlaylist.sh
@@ -4,12 +4,14 @@
 
 # input
 #$1=PLX80XgzEbfekiH5r_kAGfsEq6enwol_Km
+useragent='User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0'
 
-curl "https://www.youtube.com/playlist?list=$1" > list.html
+curl -L -A "$useragent" "https://www.youtube.com/playlist?list=$1" > list.html
 title=`cat list.html | grep '<title>' | cut -d'>' -f 2 | sed 's/ //g'|sed 's/\///g'`
 cat list.html | grep pl-video-title-link | sed 's/v=/*/g' | cut -d'*' -f 2 | cut -d'&' -f 1 > list.txt
+mkdir "$1-$title"
 cnt=1
 for fn in `cat list.txt`; do
-    bash getYoutube.sh $fn "$1-$title" $cnt
+    bash getYoutube.sh $fn "$1-$title" `printf "%03d\n" $cnt`
     cnt=$((cnt+1))
 done
--- getYoutube.sh
+++ getYoutube.sh
@@ -10,16 +10,32 @@
 # set resolution here
 # 22=720p, 18=360p, 36=240p, 17=144p, 43=360p(webm)
 video_res=18
+useragent='User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0'
 
 if [ -z "$2" ] 
 then
   head=""
 else
-  head=$2-$3-
+  head=$2"/"$3-
 fi
-curl http://www.youtube.com/get_video_info?video_id=$1 > get_video_info
-title=`cat get_video_info | sed 's/%26/&/g'| tr '&' '\n' | grep title | cut -c 7- | gawk -vRS='%[0-9a-fA-F]{2}' 'RT{sub("%","0x",RT);RT=sprintf("%c",strtonum(RT))} {gsub(/\+/," ");printf "%s", $0 RT}'|sed 's/ //g'|sed 's/\///g'`
-url=`cat get_video_info | sed 's/%26/&/g'| tr '&' '\n' | grep url_encoded_fmt_stream_map |gawk -vRS='%[0-9a-fA-F]{2}' 'RT{sub("%","0x",RT);RT=sprintf("%c",strtonum(RT))} {gsub(/\+/," ");printf "%s", $0 RT}'|  sed 's/url\=/\&/g' | tr '&' '\n' | grep itag%3D$video_res | gawk -vRS='%[0-9a-fA-F]{2}' 'RT{sub("%","0x",RT);RT=sprintf("%c",strtonum(RT))} {gsub(/\+/," ");printf "%s", $0 RT}'`
-echo $url > url.txt  #debug
-curl "$url" > "$1.mp4"
-mv "$1.mp4" "$head$1-$title.mp4"
+curl -L -A "$useragent" "http://www.youtube.com/get_video_info?video_id=$1" > get_video_info
+title=`cat get_video_info | sed 's/%26/&/g'| tr '&' '\n' | grep title= | cut -c 7- | python urldecode.py |sed 's/ //g'|sed 's/\///g'`
+cat get_video_info | sed 's/%26/&/g'| tr '&' '\n' | grep url_encoded_fmt_stream_map | python urldecode.py |  sed 's/url\=/\&/g' | tr '&' '\n' | python urldecode.py | grep http > /tmp/urls.tmp
+cat /tmp/urls.tmp | grep itag=$video_res > urls.txt
+cat /tmp/urls.tmp | grep -v itag=$video_res >> urls.txt
+#url=`cat get_video_info | sed 's/%26/&/g'| tr '&' '\n' | grep url_encoded_fmt_stream_map | python urldecode.py |  sed 's/url\=/\&/g' | tr '&' '\n' | grep itag%3D$video_res | python urldecode.py`
+#echo $url > url.txt  #debug
+for fn in `cat urls.txt`; do
+  echo $fn  #debug
+  status=`curl -L -A "$useragent" -w "%{http_code}" -o "$head$1.mp4" "$fn"`
+  #status="404"
+  #echo $status  #debug
+  if [ $status -eq "403" ]
+    then
+    echo "403-try next"
+    else
+    echo "get"
+    break
+  fi
+done
+mv "$head$1.mp4" "$head$1-$title.mp4"
