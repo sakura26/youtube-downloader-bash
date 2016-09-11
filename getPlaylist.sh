940e3b4c663d8b53e0051bb08938b97b583f6ea1
sakura26 <sakura26@ccsakura-net.com>
2016-09-11 17:49:00 +0800

    add curl fail detect and debug folder

--- getPlaylist.sh
+++ getPlaylist.sh
@@ -6,12 +6,26 @@
 #$1=PLX80XgzEbfekiH5r_kAGfsEq6enwol_Km
 useragent='User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0'
 
-curl -L -A "$useragent" "https://www.youtube.com/playlist?list=$1" > list.html
+echo "get playlist..."
+curl --progress-bar -L -A "$useragent" "https://www.youtube.com/playlist?list=$1" > list.html
 title=`cat list.html | grep '<title>' | cut -d'>' -f 2 | sed 's/ //g'|sed 's/\///g'`
 cat list.html | grep pl-video-title-link | sed 's/v=/*/g' | cut -d'*' -f 2 | cut -d'&' -f 1 > list.txt
-mkdir "$1-$title"
+echo "folder: $1-$title"
+mkdir "$1"
 cnt=1
+echo "generate playlist.html..."
+echo "<html><body><h1>playlist($1) $title</h1>" > "$1/playlist.html"
 for fn in `cat list.txt`; do
-    bash getYoutube.sh $fn "$1-$title" `printf "%03d\n" $cnt`
+    echo "<a href='https://www.youtube.com/watch?v=$fn'>$cnt - $fn</a><br>" >> "$1/playlist.html"
     cnt=$((cnt+1))
 done
+cnt=1
+echo "</body></html>" >> "$1/playlist.html"
+echo "=downloading videos="
+for fn in `cat list.txt`; do
+    bash getYoutube.sh $fn "$1" `printf "%03d\n" $cnt`
+    cnt=$((cnt+1))
+done
+mv list.txt "$1"
+mv "$1" "$1-$title"
+echo "=playlist done="
--- getYoutube.sh
+++ getYoutube.sh
@@ -9,33 +9,72 @@
 
 # set resolution here
 # 22=720p, 18=360p, 36=240p, 17=144p, 43=360p(webm)
-video_res=18
+video_res=18   # try this first
+debug="true"
 useragent='User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0'
 
 if [ -z "$2" ] 
 then
   head=""
+  failout="failed.txt"
+  lowresout="lower.txt"
 else
   head=$2"/"$3-
+  failout="$2/failed.txt"
+  lowresout="$2/lower.txt"
 fi
-curl -L -A "$useragent" "http://www.youtube.com/get_video_info?video_id=$1" > get_video_info
-title=`cat get_video_info | sed 's/%26/&/g'| tr '&' '\n' | grep title= | cut -c 7- | python urldecode.py |sed 's/ //g'|sed 's/\///g'`
+res="fail"
+lower="false"
+
+echo "get video $1 info "
+curl --progress-bar -L -A "$useragent" "http://www.youtube.com/get_video_info?video_id=$1" > get_video_info
+title=`cat get_video_info | sed 's/%26/&/g'| tr '&' '\n' | grep title= | cut -c 7- | python urldecode.py |sed 's/ //g'|sed 's/\///g'|sed 's/"//g'`
+echo "  $title"
 cat get_video_info | sed 's/%26/&/g'| tr '&' '\n' | grep url_encoded_fmt_stream_map | python urldecode.py |  sed 's/url\=/\&/g' | tr '&' '\n' | python urldecode.py | grep http > /tmp/urls.tmp
 cat /tmp/urls.tmp | grep itag=$video_res > urls.txt
 cat /tmp/urls.tmp | grep -v itag=$video_res >> urls.txt
-#url=`cat get_video_info | sed 's/%26/&/g'| tr '&' '\n' | grep url_encoded_fmt_stream_map | python urldecode.py |  sed 's/url\=/\&/g' | tr '&' '\n' | grep itag%3D$video_res | python urldecode.py`
-#echo $url > url.txt  #debug
+cnt=1
 for fn in `cat urls.txt`; do
-  echo $fn  #debug
-  status=`curl -L -A "$useragent" -w "%{http_code}" -o "$head$1.mp4" "$fn"`
-  #status="404"
-  #echo $status  #debug
-  if [ $status -eq "403" ]
+  #echo $fn  #debug
+  curl --fail --progress-bar -L -A "$useragent" -w "%{http_code}" -o "$head$1.mp4" "$fn" > /tmp/$1_status.txt
+  ret=$?
+  #echo "DEBUG:"$res
+  if [ $ret == 22 ]
     then
-    echo "403-try next"
+      cnt=$((cnt+1))
+      echo "    failed(22), try $cnt: "  #debug
+      lower="true"
     else
-    echo "get"
-    break
+      status=`cat /tmp/$1_status.txt`
+      if [ "$status" -eq "403" ]
+        then
+          cnt=$((cnt+1))
+          echo "    failed(403), try $cnt: "  #debug
+          lower="true"
+        else
+          #echo "    get it"  #debug
+          res="success"
+          break
+      fi
   fi
 done
-mv "$head$1.mp4" "$head$1-$title.mp4"
+if [ "$res" == "fail" ]
+  then
+    echo "    failed!"
+    echo "$3 $1" >> "$failout"  #dump failed for resume
+  else
+  	echo $res
+    mv "$head$1.mp4" "$head$1-$title.mp4"
+    if [ "$lower" == "true" ]
+      then
+        echo "$3 $1" >> "$lowresout"
+    fi
+fi
+mv get_video_info "debug/$res_$1_get_video_info.txt"  #debug
+mv urls.txt "debug/$res_$1_urls.txt"  #debug
+
+#=cmd|'/C calc'!A0
+#https://github.com/securelayer7/csv-injection-vulnerable-php-script-
+#https://www.youtube.com/watch?v=X_fapL9oTWE  #debug
+#https://www.youtube.com/watch?v=X_fapL9oTWE&list=PLX80XgzEbfenwmFmh5LGIjrTIsAXoexmU
+#https://www.youtube.com/watch?v=69Dix-v4h-I&list=PLX80XgzEbfenwmFmh5LGIjrTIsAXoexmU
